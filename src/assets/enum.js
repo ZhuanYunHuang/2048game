13162f8866f98dd7ce93b297de56aa24be5d6cbe
huangyz <huangyz@adnonstop.com.cn>
2024-06-26 17:50:44 +0800

    功能实现,但是复杂度很高,偶先bug不知道什么情况

--- src/assets/enum.js
+++ src/assets/enum.js
@@ -21,3 +21,94 @@ export const getBgColor = (number) => {
 export const getNumColor = (number) => {
   return number <= 4 ? '#776e65' : '#fff'
 }
+
+export const getFontSize = (number) => {
+  if(number < 128) {
+    return 30
+  } else if(number < 1024) {
+    return 28
+  } else if(number >= 1024) {
+    return 20
+  }
+}
+
+//防抖函数
+export const debounce = (func, delay, immediate) => {
+  let timer;
+  return function () {
+    if (timer) clearTimeout(timer);
+    if (immediate) {
+      // 复杂的防抖函数
+      // 判断定时器是否为空，如果为空，则会直接执行回调函数
+      let firstRun = !timer;
+      // 不管定时器是否为空，都会重新开启一个新的定时器,不断输入，不断开启新的定时器，当不在输入的delay后，再次输入就会立即执行回调函数
+      timer = setTimeout(() => {
+        timer = null;
+      }, delay);
+      if (firstRun) {
+        func.apply(this, arguments);
+      }
+      // 简单的防抖函数
+    } else {
+      timer = setTimeout(() => {
+        func.apply(this, arguments);
+      }, delay);
+    }
+  };
+}
+ 
+/**
+ * 节流函数
+ * @param {*} fn 要执行的函数
+ * @param {*} interval 时间间隔
+ * @param {*} options 可选参数： leading第一次是否执行, trailing 最后一次是否执行
+ * @returns 
+ */
+export const throttle = (fn, interval, options = { leading: false, trailing: true }) => {
+  // 1.记录上一次的开始时间
+  const { leading,trailing,resultCallback} = options
+  let lastTime = 0
+  let timer = null
+ 
+  // 2.事件触发时, 真正执行的函数
+  const _throttle = function (...args) {
+      // 2.1.获取当前事件触发时的时间
+      const nowTime = new Date().getTime()
+      if (!lastTime && !leading) lastTime = nowTime
+ 
+      // 2.2.使用当前触发的时间和之前的时间间隔以及上一次开始的时间, 计算出还剩余多长事件需要去触发函数
+      const remainTime = interval - (nowTime - lastTime)
+      if (remainTime <= 0) {
+        if (timer) {
+          clearTimeout(timer)
+          timer = null
+        }
+ 
+        // 2.3.真正触发函数
+        const result = fn.apply(this, args)
+        if (resultCallback && typeof resultCallback === 'function') 
+           resultCallback(result) //函数回调返回返回值
+        // 2.4.保留上次触发的时间
+        lastTime = nowTime
+        return
+      }
+ 
+      if (trailing && !timer) {
+        timer = setTimeout(() => {
+          const result = fn.apply(this, args)
+          if (resultCallback  && typeof resultCallback === 'function')  
+           resultCallback(result) //函数回调返回返回值
+          timer = null
+          lastTime = !leading ? 0 : new Date().getTime()
+        }, remainTime)
+      }
+  }
+ 
+  _throttle.cancel = function () {
+    if (timer) clearTimeout(timer)
+    timer = null
+    lastTime = 0
+  }
+ 
+  return _throttle
+}
--- src/main/MainHome.vue
+++ src/main/MainHome.vue
@@ -7,18 +7,18 @@
           <span style="margin-top: 6px;">{{ totalGoal }}</span>
         </div>
         <div> 
-          <div @click="upHandle">上</div>
-          <div>下</div>
-          <div>左</div>
-          <div>右</div>
+          <div @click="baseHandle(1)">上</div>
+          <div @click="baseHandle(2)">下</div>
+          <div @click="baseHandle(3)">左</div>
+          <div @click="baseHandle(4)">右</div>
         </div>
       </div>
       <div class="score-box begin" @click="init">开始</div>
      </div>
      <div class="section">
       <tr v-for="(rowItem, rowIndex) in cellList" :key="rowIndex" class="cell-box flexMainXYcenter">
-        <td v-for="cellItem in rowItem" :key="cellItem.id" class="cell flexMainXcenter" :style="`color: ${getNumColor(cellItem.value)}; background: ${getBgColor(cellItem.value)}`">
-          {{ cellItem.value }}
+        <td v-for="cellItem in rowItem" :key="cellItem.id" class="cell flexMainXcenter" :style="`color: ${getNumColor(cellItem.value)}; background: ${getBgColor(cellItem.value)}; font-size: ${getFontSize(cellItem.value)}px`">
+          {{ cellItem.value === 0 ? '' : cellItem.value }}
         </td>
       </tr>
      </div>
@@ -27,7 +27,7 @@
 
 <script setup>
 import { ref, onMounted, computed } from 'vue'
-import { getNumColor, getBgColor } from '@/assets/enum'
+import { getNumColor, getBgColor, getFontSize, throttle } from '@/assets/enum'
 
 const cellList = ref([
   [ { id: '0-0', value: 0 }, { id: '0-1', value: 0 }, { id: '0-2', value: 0 }, { id: '0-3', value: 0 } ],
@@ -54,28 +54,30 @@ const init = () => {
 //随机生产方块
 const generateCell = (_num, _value) => {
   const num = _num || Math.floor(Math.random() * 2) + 1 //随机生成1个或2个方块
-  const canList = ['0-0', '1-0'] //可以生成的格子列表(value为0)
-  // cellList.value.forEach((item) => {
-  //   item.forEach((_item) => {
-  //     if(_item.value === 0) {
-  //       canList.push(_item.id)
-  //     }
-  //   })
-  // })
+  // const canList = ['0-0', '1-0'] //可以生成的格子列表(value为0)
+  const canList = [] //可以生成的格子列表(value为0)
+  cellList.value.forEach((item) => {
+    item.forEach((_item) => {
+      if(_item.value === 0) {
+        canList.push(_item.id)
+      }
+    })
+  })
   let n = num
   let canListLen = canList.length
 
   const newCellList = []
-  while(n--) {
+  while(n-- && canListLen) {
     const cellData = {
       id: null,
       value: _value || (Math.floor(Math.random() * 10) > 8 ? 4 : 2) //生成2的概率80，4的概率20
     }
-    let newCell = Math.floor(Math.random() * canListLen)
-    while(newCellList.find(item => item.id === newCell)) {
-      newCell = Math.floor(Math.random() * canListLen) //防止生成在同一格子
+    let newCell = canList[Math.floor(Math.random() * canListLen)]
+    while(newCellList.find(item => item.id == newCell)) {
+      newCell = canList[Math.floor(Math.random() * canListLen)] //防止生成在同一格子
     }
-    cellData.id = canList[newCell]
+    console.log('vvvv', newCell);
+    cellData.id = newCell
     newCellList.push(cellData)
     if(canListLen === 1) break
   }
@@ -88,7 +90,14 @@ const generateCell = (_num, _value) => {
       })
     })
   })
-  console.log(newCellList);
+  console.log('新值', ...newCellList);
+  cellList.value.forEach((i, index) => {
+    let s = ''
+    i.forEach((item) => {
+      s += item.value + ','
+    })
+    console.log('第'+ index +'行', ...s);
+  })
 }
 
 //滑动后是否有值改变，不改变则不生成新方块
@@ -96,6 +105,7 @@ const isChangeCell = (newList, oldList) => {
   for (let i = 0; i < oldList.length; i++) {
     for (let j = 0; j < oldList[i].length; j++) {
       if(oldList[i][j].value !== newList[i][j].value) {
+        console.log('改变了');
         return true
       }
     }  
@@ -103,34 +113,156 @@ const isChangeCell = (newList, oldList) => {
   return false
 }
 
+const baseHandle = throttle((arrow) => {
+  const oldCellList = JSON.parse(JSON.stringify(cellList.value))
+  if(arrow === 1) upHandle() 
+  if(arrow === 2) downHandle()
+  if(arrow === 3) leftHandle()
+  if(arrow === 4) rightHandle() 
+  if(isChangeCell(cellList.value, oldCellList)) {
+    setTimeout(generateCell, 250)
+  }
+}, 250, { leading: true, trailing: false })
+
 //向上滑动
 const upHandle = () => {
-  const oldCellList = JSON.parse(JSON.stringify(cellList.value))
-  for (let col = 0; col < cellList.value[0].length; col++) {
+  for (let col = 0; col <= cellList.value[0].length - 1; col++) {
     for(let raw = 0; raw <= cellList.value.length - 1; raw++) {
-      // const currentCell = cellList.value[raw][col]
       let n = cellList.value.length - 1 - raw
       while(n--) {
         if(cellList.value[raw][col].value === 0) { //如果有0值则后面的值都推前一位
           for(let e = raw; e <= cellList.value.length - 2; e++) {
             cellList.value[e][col].value = cellList.value[e+1][col].value
           }
-        } else if(cellList.value[raw][col].value === cellList.value[raw+1][col].value) { //如果后一位值和当前值相等则相加，后面的值都推前一位
-          cellList.value[raw][col].value += cellList.value[raw+1][col].value
-          for(let e = raw+1; e <= cellList.value.length - 2; e++) {
-            cellList.value[e][col].value = cellList.value[e+1][col].value
-          }
+          cellList.value[cellList.value.length - 1][col].value = 0 //最后一个用0补充
+        } else {
+          break
         }
       }
     }
   }
-  if(isChangeCell(cellList.value, oldCellList)) {
-    generateCell()
+  for (let col = 0; col <= cellList.value[0].length - 1; col++) {
+    for(let raw = 0; raw <= cellList.value.length - 2; raw++) {
+      if(cellList.value[raw][col].value != 0 && cellList.value[raw][col].value === cellList.value[raw+1][col].value) { //如果后一位值和当前值相等则相加，后面的值都推前一位
+        cellList.value[raw][col].value += cellList.value[raw+1][col].value
+        for(let e = raw+1; e <= cellList.value.length - 2; e++) {
+          cellList.value[e][col].value = cellList.value[e+1][col].value
+        }
+        cellList.value[cellList.value.length - 1][col].value = 0 //最后一个用0补充
+      }
+    }
   }
+}
+
+
+//向下滑动
+const downHandle = () => {
+  for (let col = cellList.value[0].length - 1; col >= 0; col--) {
+    for(let raw = cellList.value.length - 1; raw >= 0; raw--) {
+      let n = raw
+      while(n--) {
+        if(cellList.value[raw][col].value === 0) { //如果有0值则后面的值都推前一位
+          for(let e = raw; e >= 1; e--) {
+            cellList.value[e][col].value = cellList.value[e-1][col].value
+          }
+          cellList.value[0][col].value = 0 //最后一个用0补充
+        }
+      }
+    }
+  }
+  for (let col = cellList.value[0].length - 1; col >= 0; col--) {
+    for(let raw = cellList.value.length - 1; raw >= 1; raw--) {
+      if(cellList.value[raw][col].value != 0 && cellList.value[raw][col].value === cellList.value[raw-1][col].value) { //如果后一位值和当前值相等则相加，后面的值都推前一位
+        cellList.value[raw][col].value += cellList.value[raw-1][col].value
+        for(let e = raw-1; e >= 1; e--) {
+          cellList.value[e][col].value = cellList.value[e-1][col].value
+        }
+        cellList.value[0][col].value = 0 //最后一个用0补充
+      }
+    }
+  }
+}
+
+//向左滑动
+const leftHandle = () => {
+  for(let raw = 0; raw <= cellList.value.length - 1; raw++) {
+    for (let col = 0; col <= cellList.value[0].length - 1; col++) {
+      let n = cellList.value.length - 1 - col
+      while(n--) {
+        if(cellList.value[raw][col].value === 0) { //如果有0值则后面的值都推前一位
+          for(let e = col; e <= cellList.value[0].length - 2; e++) {
+            cellList.value[raw][e].value = cellList.value[raw][e+1].value
+          }
+          cellList.value[raw][cellList.value[0].length - 1].value = 0 //最后一个用0补充
+        }
+      }
+    }
+  }
+  for(let raw = 0; raw <= cellList.value.length - 1; raw++) {
+    for (let col = 0; col <= cellList.value[0].length - 2; col++) {
+      if(cellList.value[raw][col].value != 0 && cellList.value[raw][col].value === cellList.value[raw][col+1].value) { //如果后一位值和当前值相等则相加，后面的值都推前一位
+        cellList.value[raw][col].value += cellList.value[raw][col+1].value
+        for(let e = col+1; e <= cellList.value[0].length - 2; e++) {
+          cellList.value[raw][e].value = cellList.value[raw][e+1].value
+        }
+        cellList.value[raw][cellList.value[0].length - 1].value = 0 //最后一个用0补充
+      }
+    }
+  }
+}
+
+//向右滑动
+const rightHandle = () => {
+  for(let raw = cellList.value.length - 1; raw >= 0; raw--) {
+    for (let col = cellList.value[0].length - 1; col >= 0; col--) {
+      let n = col
+      while(n--) {
+        if(cellList.value[raw][col].value === 0) { //如果有0值则后面的值都推前一位
+          for(let e = col; e >=1; e--) {
+            cellList.value[raw][e].value = cellList.value[raw][e-1].value
+          }
+          cellList.value[raw][0].value = 0 //最后一个用0补充
+        }
+      }
+    }
+  }
+  for(let raw = cellList.value.length - 1; raw >= 0; raw--) {
+    for (let col = cellList.value[0].length - 1; col >= 1; col--) {
+      if(cellList.value[raw][col].value != 0 && cellList.value[raw][col].value === cellList.value[raw][col-1].value) { //如果后一位值和当前值相等则相加，后面的值都推前一位
+        cellList.value[raw][col].value += cellList.value[raw][col-1].value
+        for(let e = col-1; e >= 1; e--) {
+          cellList.value[raw][e].value = cellList.value[raw][e-1].value
+        }
+        cellList.value[raw][0].value = 0 //最后一个用0补充
+      }
+    }
+  }
+}
+
+
+//键盘操作
+const keyHandle = () => {
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'ArrowUp') {
+    // 用户按下了上箭头键
+    baseHandle(1)
+  } else if (event.key === 'ArrowDown') {
+    // 用户按下了下箭头键
+    baseHandle(2)
+  } else if (event.key === 'ArrowLeft') {
+    // 用户按下了左箭头键
+    baseHandle(3)
+  } else if (event.key === 'ArrowRight') {
+    // 用户按下了右箭头键
+    baseHandle(4)
+  }
+});
+
 }
 
 onMounted(() => {
   init()
+  keyHandle()
 })
 
 </script>
@@ -167,8 +299,11 @@ onMounted(() => {
         width: 25%;
         aspect-ratio: 1;
         margin: 5px;
+        font-size: 36px;
+        font-weight: bold;
         background: #C8BEB2;
         border-radius: 6px;
+        transition: all .2s;
       }
     }
   }
